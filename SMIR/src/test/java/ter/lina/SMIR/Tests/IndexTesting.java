bb629e7bc85bb2477d8888caf032665634bf86dd
sbelassa <salma.belassal@gmail.com>
2017-04-20 00:23:44 +0200

    searching wooooorks!

---
+++ SMIR/src/test/java/ter/lina/SMIR/Tests/IndexTesting.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ter.lina.SMIR.Tests;
+
+/**
+ * @author salma
+ *
+ */
+public class IndexTesting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- SMIR/src/test/java/ter/lina/SMIR/Tests/SearchTesting.java
+++ SMIR/src/test/java/ter/lina/SMIR/Tests/SearchTesting.java
@@ -1,12 +1,17 @@
+/**
+ * 
+ */
 package ter.lina.SMIR.Tests;
 
 /**
- * 
  * @author salma
  *
  */
-public class IndexingTest {
+public class SearchTesting {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
